0e16253e64c8422181378162096147e960c862b6
futoricky <ricardoamercado@gmail.com>
2016-04-13 21:57:39 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+var randString = function()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
+
+
+var registerReferral = function(url, refAmount) {
+  var nightmare = new Nightmare({show: false});
+  nightmare
+  .goto(url)
+  .type('input[name="first_name"]', randString())
+  .type('input[name="email"]', randString() + '@hotmail.com')
+  .click('.pform .btn-container button')
+  .end()
+  .then(function() {
+    console.log("Referral process #", processCount + 1, " done");
+    processCount++;
+    if (processCount < refAmount) {
+      registerReferral(url, refAmount);
+    } else {
+      console.log("Referral Process Over");
+    }
+  });
+};
+
+var processCount = 0;
+var prompt = require('prompt');
+var Nightmare = require('nightmare');
+
+prompt.start();
+prompt.get([{
+    name: 'url',
+    message: 'Your referral url',
+    required: true
+  }, {
+    name: 'refAmount',
+    message: 'Amount of referrals you want',
+    type: 'integer'
+  }], function (err, result) {
+    if (!result.url) {
+      console.log("Please provide valid referral url");
+    } else {
+      var refAmount = 25;
+      url = result.url;
+      if (result.refAmount) {
+        refAmount = result.refAmount;
+      }
+      console.log("Processing, please wait...");
+      registerReferral(result.url, refAmount);
+    }
+});
